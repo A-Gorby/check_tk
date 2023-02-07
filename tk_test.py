24c405f714567ec06ab75b8567564e3cd2f1f160
A-Gorby <123462010+A-Gorby@users.noreply.github.com>
2023-02-07 17:40:32 +0300

    Correction 03

--- tk_test.py
+++ tk_test.py
@@ -40,7 +40,7 @@ from manual_dictionaries import cols_chunks_02, main_cols, dtypes_chunks_dicts, 
 from manual_dictionaries import err_msg_lst
 
 from utils import find_rec_pd, find_col, find_rec_pd_by_col_names_02, test_extract_chunk_positions
-from utils import read_chunks, save_to_excel
+from utils import read_chunks, save_to_excel, load_check_dictionaries
 from utils import conv_str_lst_2_int_lst, transform_list_form_xlsx, add_check_comments
 from utils import find_rec_pd, find_col, find_rec_pd_by_col_names_02, read_chunks, test_extract_chunk_positions
 from utils import print_err_messages, get_err_messages #, check_row
